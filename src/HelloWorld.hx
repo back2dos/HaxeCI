97ba8d340d926fbbec4bbdf1f2433cfa81271ee0
Andy Li <andy@onthewings.net>
2016-10-20 13:35:23 +0800

    test

--- src/HelloWorld.hx
+++ src/HelloWorld.hx
@@ -2,6 +2,10 @@ class HelloWorld {
 	static function main():Void {
 		trace("Hello world!");
 
+		#if sys
+		Sys.command("haxelib", ["-version"]);
+		#end
+
 		#if flash
 		flash.system.System.exit(0);
 		#end
